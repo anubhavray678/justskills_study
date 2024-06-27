38a99c9158a8dc296f1adf8cc1494b00fd189805
fastbricks <64881373+anubhavray678@users.noreply.github.com>
2024-06-27 13:18:22 +0530

    update

--- src/app/read/page.jsx
+++ src/app/read/page.jsx
@@ -5,62 +5,10 @@ import MenuCategories from "@/components/menuCategories/MenuCategories";
 const BlogPage = ({ searchParams }) => {
   const page = parseInt(searchParams.page) || 1;
   const { cat } = searchParams;
-  const read =
-    "https://www.springboard.com/blog/wp-content/uploads/2022/09/programmng-language.jpg";
-  const images = [
-    { id: "1", category: "android", imgUrl: "/culture.png" },
-    { id: "2", category: "emerging-tech", imgUrl: "/coding.png" },
-    {
-      id: "5",
-      category: "coding",
-      imgUrl:
-        "https://www.springboard.com/blog/wp-content/uploads/2022/09/programmng-language.jpg",
-    },
-    { id: "4", category: "placement", imgUrl: "/read.png" },
-  ];
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{cat} : Read different topics</h1>
-      {/* <img
-        src="https://blog.hubspot.com/hs-fs/hubfs/202_Reasons-Consumers-Read-Blogs.png?width=595&height=400&name=202_Reasons-Consumers-Read-Blogs.png"
-        style={{
-          objectFit: "fill",
-          width: "100%",
-          height: "400px",
-          display: `${css}`,
-        }}
-      /> */}
-      <div
-        style={{
-          backgroundColor: "",
-          display: "flex",
-          justifyContent: "center",
-          objectFit: "cover",
-          height: "300px",
-        }}
-      >
-        {images.map((id) => {
-          if (id.category === cat) {
-            var imgNew = id.imgUrl;
-            var newId = id;
-          } else {
-            return <></>;
-          }
-          return (
-            // <div>
-            <img
-              key={newId}
-              src={imgNew}
-              alt=""
-              className="invisible"
-              style={{ objectFit: "cover", width: "100%", height: "100%" }}
-            />
-            // </div>
-          );
-        })}
-      </div>
-
       <div className={styles.content}>
         <CardList page={page} cat={cat} />
         <MenuCategories />
--- src/components/post/PostPage.js
+++ src/components/post/PostPage.js
@@ -4,6 +4,7 @@ import styles from "@/app/posts/[slug]/singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 import MenuCategories from "../menuCategories/MenuCategories";
+import { FaShareAlt } from "react-icons/fa";
 
 const getData = async (slug) => {
   const res = await fetch(`https://study.justskills.in/api/posts/${slug}`, {
@@ -40,23 +41,29 @@ function PostPage({ params }) {
         <div className={styles.infoContainer}>
           <div className={styles.textContainer}>
             <h1 className={styles.title}>{data.title}</h1>
-            <div className={styles.user}>
-              {data.user?.image && (
-                <div className={styles.userImageContainer}>
-                  <Image
-                    src={data.user.image}
-                    alt=""
-                    fill
-                    className={styles.avatar}
-                  />
+            <div className="flex justify-between items-center">
+              <div className={styles.user}>
+                {data.user?.image && (
+                  <div className={styles.userImageContainer}>
+                    <Image
+                      src={data.user.image}
+                      alt=""
+                      fill
+                      className={styles.avatar}
+                    />
+                  </div>
+                )}
+                <div className={styles.userTextContainer}>
+                  <span className={styles.username}>{data.user.name}</span>
+                  <span className={styles.date}>
+                    {data.createdAt.substring(0, 10)}
+                  </span>
                 </div>
-              )}
-              <div className={styles.userTextContainer}>
-                <span className={styles.username}>{data.user.name}</span>
-                <span className={styles.date}>
-                  {data.createdAt.substring(0, 10)}
-                </span>
               </div>
+
+              <button className="bg-green-600">
+                Share <FaShareAlt />
+              </button>
             </div>
           </div>
           {data.img && (
