91d5b8f100f7c07e9a4c139e5b68f274246cead0
fastbricks <64881373+anubhavray678@users.noreply.github.com>
2024-06-30 18:06:47 +0530

    update

--- src/app/api/latest/route.js
+++ src/app/api/latest/route.js
@@ -1,82 +1,43 @@
-// import prisma from "@/utils/connect";
-// import { NextResponse } from "next/server";
-
-// export const GET = async (req) => {
-//   try {
-//     // Function to fetch all blog post data
-//     const fetchBlogPosts = async () => {
-//       const posts = await prisma.post.findMany();
-//       return posts;
-//     };
-
-//     // Fetch blog post data
-//     const posts = await fetchBlogPosts();
-
-//     // Return the posts in JSON format with a status code of 200
-//     return NextResponse.json(posts, { status: 200 });
-//   } catch (error) {
-//     console.error("Error fetching blog posts:", error);
-
-//     // Return an error message in JSON format with a status code of 500
-//     return NextResponse.json(
-//       { message: "Something went wrong!" },
-//       { status: 500 }
-//     );
-//   }
-// };
-
-// // Polling mechanism to fetch data every minute
-// const interval = setInterval(async () => {
-//   try {
-//     const posts = await fetchBlogPosts();
-//     console.log("Updated blog posts:", posts);
-//     // Log the updated posts
-//   } catch (error) {
-//     console.error("Error fetching blog posts:", error);
-//   }
-// }, 30000);
-// // Fetch data every 60 seconds
-
-// // Clean up the interval on server shutdown
-// export const onServerShutdown = () => {
-//   clearInterval(interval);
-// };
-
-// pages/api/posts.js
-import clientPromise from "@/utils/connectMongo";
+import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
   try {
-    const client = await clientPromise;
-    const db = client.db("BlogAPP"); // Replace with your database name
-    const posts = await db.collection("Post").find({}).toArray();
+    // Function to fetch all blog post data
+    const fetchBlogPosts = async () => {
+      const posts = await prisma.post.findMany();
+      return posts;
+    };
 
-    return NextResponse.json(posts, {
-      status: 200,
-      headers: {
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
-        "Surrogate-Control": "no-store",
-      },
-    });
+    // Fetch blog post data
+    const posts = await fetchBlogPosts();
+
+    // Return the posts in JSON format with a status code of 200
+    return NextResponse.json(posts, { status: 200 });
   } catch (error) {
     console.error("Error fetching blog posts:", error);
 
+    // Return an error message in JSON format with a status code of 500
     return NextResponse.json(
       { message: "Something went wrong!" },
-      {
-        status: 500,
-        headers: {
-          "Cache-Control":
-            "no-store, no-cache, must-revalidate, proxy-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-          "Surrogate-Control": "no-store",
-        },
-      }
+      { status: 500 }
     );
   }
+};
+
+// Polling mechanism to fetch data every minute
+const interval = setInterval(async () => {
+  try {
+    const posts = await fetchBlogPosts();
+    console.log("Updated blog posts:", posts);
+    // Log the updated posts
+  } catch (error) {
+    console.error("Error fetching blog posts:", error);
+  }
+}, 30000);
+// Fetch data every 60 seconds
+
+// Clean up the interval on server shutdown
+export const onServerShutdown = () => {
+  clearInterval(interval);
 };
